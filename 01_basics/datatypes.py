0e626504f324e56998fa160d54fbbd34645e82a8
SubhamayDebnath <subhamaydebnath@gmail.com>
2025-03-03 21:21:53 +0530

    "Updated print statement in index.py"

---
+++ 01_basics/datatypes.py
@@ -0,0 +1,20 @@
+#1.number
+num= 12
+# 2.string
+str = "hello"
+# 3.list
+lst = [1,2,3,4,5]
+# 4.tuple
+tup = (1,2,3,4,5)
+# 5.dictionary
+dct = {"name":"John","age":30}
+# 6.set
+set = {1,2,3,4,5}
+#7.boolean
+bool = True
+#8.float
+flt = 12.5
+#9.complex
+cpl = 12+5j
+#10.None
+none = None
