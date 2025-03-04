212cf05c15883b6b06cf9ac500aa13764fd53ad0
SubhamayDebnath <subhamaydebnath@gmail.com>
2025-03-04 14:35:09 +0530

    `Added documentation for built-in Python data types`

--- 01_basics/datatypes.py
+++ 01_basics/datatypes.py
@@ -18,3 +18,34 @@ flt = 12.5
 cpl = 12+5j
 #10.None
 none = None
+
+
+# note
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# x = "Hello World"	str	
+# x = 20	int	
+# x = 20.5	float	
+# x = 1j	complex	
+# x = ["apple", "banana", "cherry"]	list	
+# x = ("apple", "banana", "cherry")	tuple	
+# x = range(6)	range	
+# x = {"name" : "John", "age" : 36}	dict	
+# x = {"apple", "banana", "cherry"}	set	
+# x = frozenset({"apple", "banana", "cherry"})	frozenset	
+# x = True	bool	
+# x = b"Hello"	bytes	
+# x = bytearray(5)	bytearray	
+# x = memoryview(bytes(5))	memoryview	
+# x = None	NoneType
+
+# print(memoryview(bytes(5)))
+# print(bytearray(5))
---
+++ 02_Strings/strings.py
@@ -0,0 +1,12 @@
+# singleLineString = 'hello'
+# multiLineString = '''
+# hello
+# world
+# '''
+# print(multiLineString)
+
+# Strings are Arrays
+
+exmStr = 'hello world'
+# print(len(exmStr))
+# print(exmStr[1]) # e
